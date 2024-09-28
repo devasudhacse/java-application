06109c6f61059f668ac5dd4d5b144dc3ffafe8fd
Sudha <sudha12@gmail.com>
2024-09-28 19:16:09 +0530

    second commit

---
+++ Example1.java
@@ -0,0 +1,10 @@
+public class Test
+{
+public static void main(string[] args)
+{
+System.out.println("**********************");
+System.out.println("Hello world\n");
+System.out.println("**********************");
+}
+
+}
